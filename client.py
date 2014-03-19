fc7e9063bfea1d62f3a0ec780e209919d3b58198
Ryan G. Hunter <rhunter@etiometry.com>
2014-03-19 18:30:57 -0400

    modded the init scripts

--- client.py
+++
@@ -1,4 +0,0 @@
-import xmlrpclib
-
-proxy = xmlrpclib.ServerProxy("http://192.168.76.24:8000/")
-proxy.navi_sound()
--- navi_sound.py
+++ navi_sound.py
@@ -3,8 +3,7 @@ import glob, random
 
 
 def play_rand_wav(file_list):
-    pygame.mixer.pre_init(frequency=32000, size=-16, channels=2, buffer=4096)
-    pygame.init()
+    pygame.mixer.init(frequency=32000, size=-16, channels=2, buffer=4096)
     pygame.mixer.Sound(random.choice(file_list)).play()
     time.sleep(0.5)
 
