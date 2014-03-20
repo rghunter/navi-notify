88ede8c648517485e2584bdfb91049f056d4be0c
Ryan G. Hunter <rhunter@etiometry.com>
2014-03-20 00:32:38 -0400

    got rid of daemon

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
     author="Ryan Hunter", author_email="rhunter@etiometry.com",
     version = VERSION,
     packages = find_packages(),
-    data_files=[('/etc/init.d/',['daemon/navi-notifyd']),
-		('/opt/navi/' , ['sound/hey.wav', 'sound/listen.wav', 'sound/hello.wav', 'sound/look.wav'])],
+    data_files=[('/opt/navi/' , ['sound/hey.wav', 'sound/listen.wav', 'sound/hello.wav', 'sound/look.wav'])],
     install_requires=['distribute','pygame'],
     scripts = ['bin/navi-notify','bin/notify']
 )
